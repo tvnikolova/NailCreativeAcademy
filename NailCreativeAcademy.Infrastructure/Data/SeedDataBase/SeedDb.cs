ac43a50b89af62513e16cc8d6ccd27b1243090cf
Tanya Nikolova <tvnikolova@abv.bg>
2024-03-27 18:00:29 +0200

    Creade seedDB class

--- NailCreativeAcademy.Infrastructure/Data/SeedDataBase/SeedDb.cs
+++ NailCreativeAcademy.Infrastructure/Data/SeedDataBase/SeedDb.cs
@@ -80,31 +80,30 @@
             FirstTrainer = new Trainer
             {
                 Id = 1,
-                FirstName = "Ekaterina",
-                LastName = "Gyurova",
-                About = "Ekaterina Gyurova has been a trainer at the academy for 3 years.She is with 7 years of experience in the field of manicure." +
-                 "There are several first places from winning international manicure competitions."
+                FirstName = "Екатерина",
+                LastName = "Николова",
+                About = "Екатерина Николова е обучител в академията от 3 години. Тя е със 7 години опит в областта на маникюра. " +
+                "Има няколко първи места от спечелени международни конкурси за маникюр "
             };
 
             SecondTrainer = new Trainer
             {
                 Id = 2,
-                FirstName = "Elena",
-                LastName = "Kushnirenko",
-                About = "Elena Kushnirenko is an international judge and trainer." +
-                "She is a master with over 10 years of experience in the field of manicure. She created programs for basic and upgrading courses. " +
-                "Elena is an international judge and multiple winner of International Nail Art Competitions."
+                FirstName = "Елена",
+                LastName = "Кушниренко",
+                About = "Елена Кушниренко е международен съдия и обучител. Тя е майстор с над 10 години опит в областта на маникюра. " +
+                "Създала е програми за основни и надграждащи курсове. " +
+                "Елена е международен съдия и многократен победител в международни конкурси за ноктопластика."
 
             };
 
             ThirdTrainer = new Trainer
             {
                 Id = 3,
-                FirstName = "Evgeniya",
-                LastName = "Makarenko",
-                About = "Evgeniya Makarenko is an manicure and pedicure trainer" +
-                "is a trainer with over 11 years of experience, certified in Ukraine. Evgeniya conducts courses in classic manicure," +
-                " classic pedicure and nail art with gel."
+                FirstName = "Евгения",
+                LastName = "Макаренко",
+                About = "Евгения Макаренко е обучител по маникюр и педикюр. Тя е с над 11 години опит и е сертифицирана в Украйна. " +
+                "Евгения провежда курсове по класически маникюр, класически педикюр и ноктопластика с гел."
             };
 
         }
@@ -114,14 +113,14 @@
             FirstType = new CourseType()
             {
                 Id = 1,
-                Name = "Begginner"
+                Name = "Начинаещи"
 
             };
 
             SecondType = new CourseType()
             {
                 Id = 2,
-                Name = "Аdvanced"
+                Name = "Напреднали"
             };
 
         }
@@ -131,13 +130,10 @@
             BasicManicureCourse = new Course()
             {
                 Id = 1,
-                Name = "Basic Manicure Course",
-                Details = "The course is for:\r\n- for beginners\r\n- for manicurists to enrich their knowledge.\r\nIt consists of a theoretical and a practical part.\r\nIt includes all the basic knowledge that any future manicurist needs.",
-                Program = "-Тheory\r\n- How to properly remove gel polish with cleaner and keep your clients nails healthy." +
-                "\r\n-Treatment of the cuticle and cuticles with a suitable preparation." +
-                "\r\n- How to choose and work with pliers and scissors." +
-                "\r\n- How to make reinforcement with a base.\r\n- How to apply gel polish close to the cuticle." +
-                "\r\nOur trainers will teach you how to keep a healthy and beautiful manicure.",
+                Name = "Базов курс по маникюр",
+                Details = "Курсът е за:\r\n- за начинаещи,\r\n- за маникюристи, които искат да обогатят знанията си." +
+                "Състои се от теоретична и практическа част. Включва всички основни знания, от които се нуждае всеки бъдещ маникюрист.\",",
+                Program = "Курсът се състои от:\r\n-Теория - 2 часа дневно\r\n Практика - 6 часа дненво: \r\n- Как правилно да сваляте гел лак с почистващ препарат и да поддържате ноктите на вашите клиенти здрави.\r\n-Третиране на кожичките с подходящ препарат.\r\n- Как да избираме и работим с клещи и ножици.\r\n- Как се прави армировка с основа.\r\n- Как да нанесете гел лак близо до кутикулата.\r\nНашите преподаватели ще ви научат как да поддържате здрав и красив маникюр.",
                 Duration = "40 часа",
                 StartDate = DateTime.Parse("2016.05.02"),
                 EndDate = DateTime.Parse("07.05.2024"),
@@ -152,7 +148,7 @@
             Sallon1 = new Saloon()
             {
                 Id = 1,
-                Address = "Pazardzhik, bul. \"Bulgariya\" 2",
+                Address = "Пазарджик, бул. \"България\" 2",
                 PhoneNumber = "0888 222 555",
                 ClientId = Client1.Id,
             };
@@ -160,7 +156,7 @@
             Sallon2 = new Saloon()
             {
                 Id = 2,
-                Address = "Sofia, pl. \"Sveta Nedelya\" 5",
+                Address = "София, пл. \"Света Неделя\" 5",
                 PhoneNumber = "0888 222 555",
                 ClientId = StudentUser1.Id,
             }; 
