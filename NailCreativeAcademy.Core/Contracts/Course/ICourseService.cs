b42b6901e517f34e4f28e94d295109dd4911906a
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-02 11:14:12 +0300

    Add method All

---
+++ NailCreativeAcademy.Core/Contracts/Course/ICourseService.cs
@@ -0,0 +1,14 @@
+﻿using NailCreativeAcademy.Core.Models.Course;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NailCreativeAcademy.Core.Contracts.Course
+{
+    public interface ICourseService
+    {
+        Task<IEnumerable<CourseViewModel>> All();
+    }
+}
---
+++ NailCreativeAcademy.Core/Services/CourseService.cs
@@ -0,0 +1,36 @@
+﻿namespace NailCreativeAcademy.Core.Services
+{
+    using Contracts.Course;
+    using Infrastructure.Data.Common;
+    using Infrastructure.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+    using Models.Course;
+    using System.Globalization;
+
+    public class CourseService:ICourseService
+    {
+        private readonly IRepository repository;
+        public CourseService(IRepository _repository)
+        {
+            this.repository = _repository;
+        }
+
+        public async Task<IEnumerable<CourseViewModel>> All()
+        {
+            var courses = await repository.AllReadOnly<Course>()
+                                           .Select(c => new CourseViewModel()
+                                           {
+                                               Name = c.Name,
+                                               StartDate = c.StartDate.ToString(CultureInfo.InvariantCulture),
+                                               Details = c.Details,
+                                               Image = c.Image,
+                                               Duration = c.Duration,
+                                               Price = c.Price
+                                           })
+                                           .ToListAsync();
+
+         return courses;
+           
+        }
+    }
+}
