eb97de17ff5d5dc3ec9e81b7d00cce4e3590c704
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-02 11:14:31 +0300

    Add method All in CourseController

--- NailCreativeAcademy/Controllers/CourseController.cs
+++ NailCreativeAcademy/Controllers/CourseController.cs
@@ -1,12 +1,65 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace NailCreativeAcademy.Controllers
+﻿namespace NailCreativeAcademy.Controllers
 {
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Core.Contracts.Course;
+    using Core.Models.Course;
+
+
+
+    [Authorize]
     public class CourseController : Controller
     {
-        public IActionResult Index()
+
+        private readonly ICourseService courseService;
+
+        public CourseController(ICourseService _courseService)
+        {
+            this.courseService = _courseService;
+        }
+
+
+        [AllowAnonymous]
+        public async Task<IActionResult> All()
+        {
+            IEnumerable<CourseViewModel> model = await courseService.All();
+
+            return View(model);
+        }
+
+        public IActionResult MyCourses()
+        {
+            return View(new AllCourseModel());
+        }
+
+        [HttpGet]
+        public IActionResult Create() 
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(CourseFormModel course)
+        {
+            return RedirectToAction(nameof(All));
+        }
+
+        [HttpGet]
+        public IActionResult ProgramAsync(int id)
+        {
+            return View(new CourseDetailsViewModel());
+        }
+
+        [HttpGet]  
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditAsync(int id, CourseFormModel course)
+        {
+            return RedirectToAction(nameof(ProgramAsync));
         }
     }
 }
