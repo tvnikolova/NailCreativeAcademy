3e53acd9063d412d88e1bd4c0b590fcae698bffb
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-03 08:48:58 +0300

    Add TrainerFromModel

---
+++ NailCreativeAcademy.Core/Models/Trainer/TrainerFormModel.cs
@@ -0,0 +1,22 @@
+﻿namespace NailCreativeAcademy.Core.Models.Trainer
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Infrastructure.Constants.NailCreativeConstants;
+    public class TrainerFormModel
+    {
+
+        [Key]
+        public int Id { get; set; }
+      
+        [Required]
+        [StringLength(TrainerDescriptionMaxLength, MinimumLength = TrainerNameMinLength,
+            ErrorMessage = "The fireld {0} is required.The name must be at least {2} symbols.")]
+        public string Name { get; set; } = string.Empty;
+        [Required]       
+
+        [StringLength(TrainerDescriptionMaxLength, MinimumLength = TrainerDescriptionMinLength,
+            ErrorMessage = "The fireld {0} is required.The name must be at least {2} and maximum {1} symbols.")]
+        public string About { get; set; } = string.Empty;
+       
+    }
+}
