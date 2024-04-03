05e5fb849cce37f0218f9ed490749e81f6acf881
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-03 08:48:23 +0300

    Add message constants in Core

---
+++ NailCreativeAcademy.Core/Constants/MessageConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace NailCreativeAcademy.Core.Constants
+{
+    public static class MessageConstants
+    {
+        public const string LengthStringRequired = "The field {0} is required. The {0} must be at lest {2} and maximum {1} symbols.";
+
+    }
+}
