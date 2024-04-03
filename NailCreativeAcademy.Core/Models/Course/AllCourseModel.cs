5b5349ffd41af34ecf01756cca0bd69176c97827
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-03 22:51:00 +0300

    Add new Message Constants

--- NailCreativeAcademy.Core/Models/Course/AllCourseModel.cs
+++ NailCreativeAcademy.Core/Models/Course/AllCourseModel.cs
@@ -2,14 +2,6 @@
 {
     public class AllCourseModel
     {
-        public string Name { get; set; } = string.Empty;
 
-        public string StartDate { get; set; } = string.Empty;
-
-        public string Duration {  get; set; } = string.Empty;
-
-        public string Trainer {  get; set; } = string.Empty;
-
-        public decimal Price { get; set; }
     }
 }
--- NailCreativeAcademy.Core/Services/CourseService.cs
+++ NailCreativeAcademy.Core/Services/CourseService.cs
@@ -1,17 +1,13 @@
 ﻿namespace NailCreativeAcademy.Core.Services
 {
-
-    using Microsoft.EntityFrameworkCore;
-    using System.Globalization;
-    using Models.Course;
-    using Contracts;
-    using Models.Trainer;
+    using Contracts.Course;
     using Infrastructure.Data.Common;
     using Infrastructure.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+    using Models.Course;
+    using System.Globalization;
 
-    using static Infrastructure.Constants.NailCreativeConstants;
-
-    public class CourseService : ICourseService
+    public class CourseService:ICourseService
     {
         private readonly IRepository repository;
         public CourseService(IRepository _repository)
@@ -21,7 +17,6 @@
 
         public async Task<IEnumerable<CourseViewModel>> All()
         {
-           
             var courses = await repository.AllReadOnly<Course>()
                                            .Select(c => new CourseViewModel()
                                            {
@@ -30,53 +25,12 @@
                                                Details = c.Details,
                                                Image = c.Image,
                                                Duration = c.Duration,
-                                               Price = c.Price,
-                                               TrainerId = c.TrainerId,
-                                              
+                                               Price = c.Price
                                            })
                                            .ToListAsync();
 
-            return courses;
+         return courses;
            
-        }
-
-        public async Task<int> CreateAsync(CourseFormModel model, int trainerId)
-        {           
-                Course newCourse = new Course()
-                {
-                    Name = model.Name,
-                    Details = model.Details,
-                    StartDate = DateTime.ParseExact(model.StartDate, DateOProjectString, CultureInfo.InvariantCulture),
-                    EndDate = DateTime.ParseExact(model.StartDate, DateOProjectString, CultureInfo.InvariantCulture),
-                    Duration = model.Duration,
-                    Price = model.Price,
-                    CourseTypeId= model.CourseTypeId,
-                    TrainerId = trainerId,
-                    Image = model.Image,
-                };
-
-            await repository.AddAsync(newCourse);
-            await repository.SaveChangesAsync();
-
-            return newCourse.Id;
-        }
-
-        public async Task<IEnumerable<CourseTypeViewModel>> GetCourseTypesAsync()
-        {
-            IEnumerable<CourseTypeViewModel> allTypes = await repository.AllReadOnly<CourseType>()
-                                                .Select(c => new CourseTypeViewModel()
-                                                {
-                                                    Id = c.Id,
-                                                    Name = c.Name
-                                                })
-                                                .ToListAsync();
-            return allTypes;
-        }
-        
-        public async Task<bool> CourseExistAsync(string courseName)
-        {
-            return await repository.AllReadOnly<Course>()
-                .AnyAsync(c => c.Name == courseName);
         }
     }
 }
