89f47f68c68ebd19fef43e06a700e7d08560b53f
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-04 07:41:05 +0300

    Add nmethod create in Course controller

--- NailCreativeAcademy/Controllers/CourseController.cs
+++ NailCreativeAcademy/Controllers/CourseController.cs
@@ -4,24 +4,31 @@
     using Microsoft.AspNetCore.Mvc;
     using Core.Contracts.Course;
     using Core.Models.Course;
-
-
+    using NailCreativeAcademy.Core.Contracts;
+    using static Infrastructure.Constants.NailCreativeConstants;
+    using static Core.Constants.MessageConstants;
+    using System.Globalization;
 
     [Authorize]
     public class CourseController : Controller
     {
 
-        private readonly ICourseService courseService;
 
-        public CourseController(ICourseService _courseService)
+        private readonly ICourseService courseService;
+        private readonly ITrainerService trainerService;
+
+        public CourseController(ICourseService _courseService,
+            ITrainerService _trainerService)
         {
             this.courseService = _courseService;
+            this.trainerService = _trainerService;
         }
 
 
         [AllowAnonymous]
         public async Task<IActionResult> All()
         {
+
             IEnumerable<CourseViewModel> model = await courseService.All();
 
             return View(model);
@@ -33,14 +40,51 @@
         }
 
         [HttpGet]
-        public IActionResult Create() 
+        public async Task<IActionResult> Create()
         {
-            return View();
+            CourseFormModel newCourse = new CourseFormModel();
+
+            newCourse.CourseTypes = await courseService.GetCourseTypesAsync();
+
+            return View(newCourse);
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateAsync(CourseFormModel course)
+        public async Task<IActionResult> Create(CourseFormModel courseModel, int trainerId)
         {
+
+            DateTime startDate = DateTime.Now;
+            DateTime endDate = DateTime.Now;
+            if (!DateTime.TryParseExact(courseModel.StartDate, DateOProjectString, CultureInfo.InvariantCulture, DateTimeStyles.None, out startDate))
+            {
+                ModelState.AddModelError(nameof(courseModel.StartDate), $"Invalid format. The correct format is {DateOProjectString}.");
+            }
+            if (!DateTime.TryParseExact(courseModel.EndDate, DateOProjectString, CultureInfo.InvariantCulture, DateTimeStyles.None, out endDate))
+            {
+                ModelState.AddModelError(nameof(courseModel.EndDate), $"Invalid format. The correct format is {DateOProjectString}.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                courseModel.CourseTypes = await courseService.GetCourseTypesAsync();
+                return View(courseModel);
+            }
+
+            if (await courseService.CourseExistAsync(courseModel.Name) == true)
+            {
+                ModelState.AddModelError(nameof(courseModel.Name), CourseExists);
+            }
+
+            if (await trainerService.TrainerExistAsync(courseModel.Trainer) == false)
+            {
+                return BadRequest();
+            }
+
+
+            trainerId = await trainerService.GetTrainerId(courseModel.Trainer);
+
+            int newCourseToAdd = await courseService.CreateAsync(courseModel, trainerId);
+
             return RedirectToAction(nameof(All));
         }
 
@@ -50,7 +94,7 @@
             return View(new CourseDetailsViewModel());
         }
 
-        [HttpGet]  
+        [HttpGet]
         public IActionResult Edit(int id)
         {
             return View();
