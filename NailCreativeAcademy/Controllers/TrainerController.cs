e1ad2afeda85650eae62f2e5bf8fe1d5154f33fc
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-04 07:40:13 +0300

    Add TrainerController after chrashed

--- NailCreativeAcademy/Controllers/TrainerController.cs
+++ NailCreativeAcademy/Controllers/TrainerController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace NailCreativeAcademy.Controllers
+﻿namespace NailCreativeAcademy.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+    using Core.Contracts.Course;
+    using Core.Contracts;
     public class TrainerController : Controller
     {
-        public IActionResult Index()
+        private readonly ITrainerService trainerService;
+        private readonly ICourseService courseService;
+        public TrainerController(ITrainerService _trainerService,
+            ICourseService _courseService)
+        {
+            this.trainerService = _trainerService;
+            this.courseService = _courseService;
+
+        }
+
+        public async Task<IActionResult> All()
         {
             return View();
+
         }
     }
 }
