0e6733a97a41f937035e5175133118ebd436f450
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-04 07:13:55 +0300

    Add new constants in Message constants

--- NailCreativeAcademy.Core/Constants/MessageConstants.cs
+++ NailCreativeAcademy.Core/Constants/MessageConstants.cs
@@ -3,6 +3,7 @@
     public static class MessageConstants
     {
         public const string LengthStringRequired = "The field {0} is required. The {0} must be at lest {2} and maximum {1} symbols.";
-
+        public const string TrainerNotExits = "The trainer is not exist. Please first add trainer!";
+        public const string CourseExists = "The course name exist!";
     }
 }
