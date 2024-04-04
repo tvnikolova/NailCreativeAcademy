6dc34dbc3df772d06069da7e8a0210de81c3d506
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-04 07:26:55 +0300

    Add Course modesl at Core.Models/Course.

--- NailCreativeAcademy.Core/Models/Course/CourseFormModel.cs
+++ NailCreativeAcademy.Core/Models/Course/CourseFormModel.cs
@@ -1,6 +1,47 @@
 ﻿namespace NailCreativeAcademy.Core.Models.Course
 {
+    using System.ComponentModel.DataAnnotations;
+    using static Infrastructure.Constants.NailCreativeConstants;
+    using static Constants.MessageConstants;
     public class CourseFormModel
     {
+
+
+        public CourseFormModel()
+        {
+            this.CourseTypes = new List<CourseTypeViewModel>();
+        }
+        [Required]
+        [StringLength(CourseNameMaxLength, MinimumLength = CourseNameMinLength,
+            ErrorMessage = LengthStringRequired)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(CourseDetailsMaxLength, MinimumLength = CourseDetailsMinLength,
+            ErrorMessage = LengthStringRequired)]
+        public string Details { get; set; } = string.Empty;
+
+        [Required]
+        public string StartDate { get; set; } = string.Empty;
+        public string? EndDate { get; set; }
+
+        [Required]
+        public string Image { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(CourseMaxDuration, MinimumLength = CourseMinDuration, ErrorMessage = LengthStringRequired)]
+        public string Duration { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int CourseTypeId { get; set; }
+
+        [Required]
+        public string Trainer { get; set; } = string.Empty;
+
+        public IEnumerable<CourseTypeViewModel> CourseTypes { get; set; }
+
     }
 }
---
+++ NailCreativeAcademy.Core/Models/Course/CourseTypeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NailCreativeAcademy.Core.Models.Course
+{
+    public class CourseTypeViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
