e1aa15b219b3d39f9ff1e5afb91fb25106fd6ede
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-08 22:00:42 +0300

    Add trainers methods All, Create, Edit, Delete in Trainer controller

--- NailCreativeAcademy/Controllers/TrainerController.cs
+++ NailCreativeAcademy/Controllers/TrainerController.cs
@@ -3,6 +3,10 @@
     using Microsoft.AspNetCore.Mvc;
     using Core.Contracts.Course;
     using Core.Contracts;
+    using NailCreativeAcademy.Core.Models.Course;
+    using NailCreativeAcademy.Core.Models.Trainer;
+    using static Core.Constants.MessageConstants;
+
     public class TrainerController : Controller
     {
         private readonly ITrainerService trainerService;
@@ -15,10 +19,91 @@
 
         }
 
+        [HttpGet]
         public async Task<IActionResult> All()
         {
-            return View();
+            IEnumerable<TrainerViewModel> model = await trainerService.AllAsync();
 
+            return View(model);
+
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var model = new TrainerFormModel();
+             return View(model);
+        
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>Create(TrainerFormModel trainer)
+        { 
+          var foundedTrainer = await trainerService.TrainerExistByNameAsync(trainer.Name);
+
+            if(foundedTrainer)
+            {
+                ModelState.AddModelError(nameof(trainer.Name), TrinerNameExists);
+            }
+            if(!ModelState.IsValid)
+            {
+                return View(trainer);
+            }
+
+            int newTrainerId = await trainerService.CreateAsync(trainer);
+
+            return RedirectToAction(nameof(All));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var foundedTrainer = await trainerService.GetTrainerFormByIdAsync(id);
+           
+            if (foundedTrainer == null)
+            {
+                return BadRequest();
+            }
+
+          
+            return View(foundedTrainer);
+        
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditAsync(TrainerFormModel trainer, int id)
+        {
+            var foundedTrainer = await trainerService.GetTrainerFormByIdAsync(id);
+
+            if (foundedTrainer == null)
+            {
+                return BadRequest();
+            }
+
+            await trainerService.EditAsync(trainer, id);
+
+            return RedirectToAction(nameof(All));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            TrainerViewModel foundTrainer = await trainerService.GetTrainerToDeleteAsync(id);
+
+            return View(foundTrainer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ConfirmDelete(int id)
+        {
+            if (!await trainerService.TrainerExistByIdAsync(id))
+            {
+                return BadRequest(ModelState);
+            }
+
+            await trainerService.DeleteAsync(id);
+
+            return RedirectToAction(nameof(All));
         }
     }
 }
