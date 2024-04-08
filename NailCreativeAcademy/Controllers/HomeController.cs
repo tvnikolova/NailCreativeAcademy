0d06b1a9a4a03db29107bfc892f5f7238e9f7030
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-08 12:54:26 +0300

    Add contacts in home page

--- NailCreativeAcademy/Controllers/HomeController.cs
+++ NailCreativeAcademy/Controllers/HomeController.cs
@@ -16,6 +16,10 @@
         {
             return View();
         }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
