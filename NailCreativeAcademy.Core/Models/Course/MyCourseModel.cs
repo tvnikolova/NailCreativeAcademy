0027b9e5e32372011680ba3b1f309b1dc7d8f19b
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-13 17:18:30 +0300

    remove unusing usings in Models

--- NailCreativeAcademy.Core/Models/Course/MyCourseModel.cs
+++ NailCreativeAcademy.Core/Models/Course/MyCourseModel.cs
@@ -3,7 +3,6 @@
     public class MyCourseModel
     {
         public int Id { get; set; }
-
         public string Name { get; set; } = string.Empty;
         public string Program {  get; set; } = string.Empty;
         public string Image { get; set; } = string.Empty;
--- NailCreativeAcademy.Core/Models/Trainer/TrainerFormModel.cs
+++ NailCreativeAcademy.Core/Models/Trainer/TrainerFormModel.cs
@@ -16,8 +16,8 @@
         [StringLength(TrainerDescriptionMaxLength, MinimumLength = TrainerNameMinLength,
             ErrorMessage = "The fireld {0} is required.The name must be at least {2} symbols.")]
         public string Name { get; set; } = string.Empty;
-        [Required]       
 
+        [Required]       
         [StringLength(TrainerDescriptionMaxLength, MinimumLength = TrainerDescriptionMinLength,
             ErrorMessage = "The fireld {0} is required.The name must be at least {2} and maximum {1} symbols.")]
         public string About { get; set; } = string.Empty;
