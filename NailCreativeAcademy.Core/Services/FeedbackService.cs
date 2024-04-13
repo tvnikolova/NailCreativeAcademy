518885a23ad18b9c384277186c55950f13eee69c
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-13 17:19:00 +0300

    Add methods in FeebbackService

---
+++ NailCreativeAcademy.Core/Services/FeedbackService.cs
@@ -0,0 +1,110 @@
+﻿namespace NailCreativeAcademy.Core.Services
+{
+    using Contracts;
+    using Microsoft.EntityFrameworkCore;
+    using Models.Feedback;
+    using NailCreativeAcademy.Core.Models.Course;
+    using NailCreativeAcademy.Infrastructure.Data.Common;
+    using NailCreativeAcademy.Infrastructure.Data.Models;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Threading.Tasks;
+
+    public class FeedbackService : IFeedbackService
+    {
+        private readonly IRepository repository;
+		
+        public FeedbackService(IRepository _repository)
+        {
+            this.repository = _repository;
+			
+        }
+		public async Task<IEnumerable<FeedbackViewModel>> AllAsync(int courseId)
+		{
+			
+			IEnumerable<FeedbackViewModel> allFeedBacks = await repository.AllReadOnly<Feedback>()
+																		.Where(c=>c.CourseId==courseId)
+																		   .Select(fb => new FeedbackViewModel()
+																		   {
+																			   Id = fb.Id,
+																			   Review = fb.Review,
+																			   ClientName = $"{fb.Client.FirstName} {fb.Client.LastName}",
+																			   CourseName = fb.Course.Name,
+																			   ClientId=fb.ClientId
+																		   })
+																		   .ToListAsync();
+
+			return allFeedBacks;
+		}
+
+		public async Task<int> AddAsync(FeedbackFormModel feedback, int courseId)
+		{
+			Feedback newFeedback = new Feedback()
+			{
+				Review = feedback.Review,
+				ClientId = feedback.ClientId,
+				CourseId = courseId,
+				
+
+			};
+				await  repository.AddAsync(newFeedback);
+				await repository.SaveChangesAsync();
+			
+			return newFeedback.Id;
+		}
+
+        public async  Task EditAsync(FeedbackFormModel model, int feedbackId)
+        {
+            var feedbackToEdit = await repository.All<Feedback>()
+                                                .Where(f => f.Id== feedbackId)
+                                                .FirstOrDefaultAsync();
+
+            if (feedbackToEdit != null)
+            {
+                feedbackToEdit.Review = model.Review;
+
+                await repository.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteAsync(int feedbackId)
+        {
+            await repository.DeleteAsync<Feedback>(feedbackId);
+            await repository.SaveChangesAsync();
+        }
+
+        public async Task<FeedbackFormModel> GetFeedbackById(int feedbackId)
+        {
+            var foundFeedback = await repository.AllReadOnly<Feedback>()
+                                                               .Where(f => f.Id == feedbackId)
+                                                               .Select(f=>new FeedbackFormModel()
+                                                               {
+                                                                   ClientId=f.ClientId,
+                                                                   CourseId=f.CourseId,
+                                                                   Review=f.Review,
+                                                               })
+                                                               .FirstAsync();
+           
+                
+                return  foundFeedback; 
+            
+        }
+
+        public async Task<FeedbackViewModel> GetFeedbackToDeleteAsync(int feedbackId)
+        {
+            var foundFeedback = await repository.AllReadOnly<Feedback>()
+                                                               .Where(f => f.Id == feedbackId)
+                                                               .Select(f => new FeedbackViewModel()
+                                                               {
+                                                                   ClientId = f.ClientId,
+                                                                   Review = f.Review,
+                                                                   CourseName=f.Course.Name,
+                                                                   ClientName= $"{f.Client.FirstName} {f.Client.LastName}"
+                                                               })
+                                                               .FirstAsync();
+
+
+            return foundFeedback;
+        }
+    }
+}
