51e4b0b83f453ecf9d297974e30ee0ad8cb3d52c
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-13 07:20:35 +0300

    Add Feedback models to Core

---
+++ NailCreativeAcademy.Core/Models/Feedback/FeedbackFormModel.cs
@@ -0,0 +1,24 @@
+﻿namespace NailCreativeAcademy.Core.Models.Feedback
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Core.Constants.MessageConstants;
+    using static Infrastructure.Constants.NailCreativeConstants;
+
+    public class FeedbackFormModel
+	{
+		
+		[Required]
+		[StringLength(FeedbackCommentMaxLength, MinimumLength = FeedbackCommentMinLength,
+			ErrorMessage = LengthStringRequired)]
+		public string Review { get; set; } = string.Empty;
+
+		[Required]		
+		public int CourseId { get; set; }
+		
+		[Required]
+		public string ClientId { get; set; } = string.Empty;
+
+        [Required]
+        public int FeedbackBoardId { get; set; } 
+    }
+}
---
+++ NailCreativeAcademy.Core/Models/Feedback/FeedbackViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NailCreativeAcademy.Core.Models.Feedback
+{
+    public class FeedbackViewModel
+    {
+
+        public int Id { get; set; }
+        public string Review { get; set; } = string.Empty;
+        public string  CourseName { get; set; } = string.Empty;
+        public string ClientName { get; set; } = string.Empty;
+
+        public string ClientId { get; set; } = string.Empty;
+
+    }
+}
