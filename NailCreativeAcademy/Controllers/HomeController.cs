1e7b762bab63fbc12c0e815f7c3a887d6eb2aea3
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-17 09:45:27 +0300

    Add Error 400

--- NailCreativeAcademy/Controllers/HomeController.cs
+++ NailCreativeAcademy/Controllers/HomeController.cs
@@ -24,7 +24,11 @@
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error(int statusCode)
         {
-            if(statusCode == 404)
+            if (statusCode == 400)
+            {
+                return View("Error400");
+            }
+            if (statusCode == 404)
             {
                 return View("Error404");
             }
