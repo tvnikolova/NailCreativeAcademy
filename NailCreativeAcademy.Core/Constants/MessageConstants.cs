902563be221166c4d8b320ad5099374d0cf77fed
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-17 20:17:08 +0300

    Create toastr and add message constants for tempdata

--- NailCreativeAcademy.Core/Constants/MessageConstants.cs
+++ NailCreativeAcademy.Core/Constants/MessageConstants.cs
@@ -12,7 +12,7 @@
         public const string SaloonAddresExists = "Салон с такъв адрес съществува!";
 		public const string SaloonPhoneNumberExists = "Салон с такъв адрес съществува!";
         public const string HasEnrolledStudentinTrainerCourse = "В курсът {0} има саписан студент. Не можете да премахнете обучителя!";
-
-
+        public const string UserAddMessageSuccess = "UserAddMessageSuccess";
+        public const string UserAddMessageError = "UserAddMessageError";
     }
 }
