7fca94d7d9a90af070127559ed6be0617c9b27ac
Tanya Nikolova <tvnikolova@abv.bg>
2024-04-17 16:21:41 +0300

    Add Admin Controller

---
+++ NailCreativeAcademy/Areas/Admin/Controllers/CourseAdminController.cs
@@ -0,0 +1,26 @@
+﻿namespace NailCreativeAcademy.Areas.Admin.Controllers
+{
+    using Core.Contracts;
+    using Microsoft.AspNetCore.Mvc;
+    using Areas.Admin.Models;
+
+    public class CourseAdminController : AdminBaseController
+    {
+        private readonly ICourseService courseService;
+        public CourseAdminController(ICourseService _courseService)
+        {
+            this.courseService = _courseService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var enrolledCourses = new EnrolledCourseModel()
+            {
+                EnrolledCourses = await courseService.GetAllCourseWithEnrolledStudentsAsync()
+            };
+
+            return View(enrolledCourses);
+        }
+    }
+}
